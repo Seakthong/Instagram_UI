f39c90e9696598deb84312dcec8f0f943b0691a2
Seakthong <seakthongaingb@gmail.com>
2019-11-21 12:54:46 +0700

    Initial Commit

---
+++ Instagram_UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram_UI
+//
+//  Created by SQ on 11/21/19.
+//  Copyright © 2019 SQ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
